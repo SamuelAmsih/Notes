16f255eb50b8a1a9bd685ee9448a1d8ab251df17
Samuel Yousef <samyousef@live.com>
2025-09-15 12:21:06 +0200

    Database setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use("/api/notes", notesRoutes);
 
+connectDB();
 
 app.listen(5005, () => {
     console.log("Server started on PORT: 5005");
 });
+
+
